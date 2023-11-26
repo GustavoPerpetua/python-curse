accc1c339cb8ef231d662872a569ac9f4f9e7b44
GustavoPerpetua <gustavoaperpe@gmail.com>
2023-11-25 21:20:29 -0300

    First commit
    First Day with this project

---
+++ introduction.py
@@ -0,0 +1,11 @@
+# I´m going to use Visual Studio Code 
+# print is a function we can use to show things in the console. 
+# To execute a funtion we need to use ().
+# For print if we need to show a string (text), we must use "", example:
+print("Hello, I´m Gustavo ")
+# To save the data we are going to use cmd+s fow macOs, ctrl+s for wndws.
+# Tip if you see in the tittle (introduction.py) a circle instead of an X in VSC, is bcz you need to save.
+# Tip open terminal with ctrl+` or ctrl+< VSC
+
+
+
