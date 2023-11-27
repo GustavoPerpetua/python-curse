f3164966cb4d1c18558ea09f65b4c5d154ba5ed4
GustavoPerpetua <gustavoaperpe@gmail.com>
2023-11-27 00:52:21 -0300

    Update introduction.py

--- introduction.py
+++ introduction.py
@@ -1,11 +1,10 @@
-# I´m going to use Visual Studio Code 
-# print is a function we can use to show things in the console. 
+# I´m going to use Visual Studio Code
+# print is a function we can use to show things in the console.
 # To execute a funtion we need to use ().
 # For print if we need to show a string (text), we must use "", example:
 print("Hello, I´m Gustavo ")
-# To save the data we are going to use cmd+s fow macOs, ctrl+s for wndws.
+# To save the data we are going to use cmd+s for macOs, ctrl+s for wndws.
 # Tip if you see in the tittle (introduction.py) a circle instead of an X in VSC, is bcz you need to save.
-# Tip open/close terminal with ctrl+` or ctrl+< VSC
+# Tip open/close terminal with ctrl + ` or ctrl + < VSC
 # To run the code we can use and extension and press the play button or we can write in the terminal python3 + files name (python3 introduction.py)
-# I recommend the next extensions on VSC: Python, Pylint
-
+# I recommend the next extensions on VSC: Python, Pylint, autopep8.
