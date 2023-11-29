b164b1628d3623e31d531ebd8b1b362f310f6a4e
GustavoPerpetua <gustavoaperpe@gmail.com>
2023-11-29 01:57:22 -0300

    sftd

--- 02-Flow-Control/3.operators.py
+++ 02-Flow-Control/3.operators.py
@@ -7,10 +7,15 @@ sparkle = True
 
 if gas and sparkle:
     print("you can use the lighter")
-# with and you need both condition True to turn on the lighter
+# with "and" you need both condition True to turn on the lighter
 
 if gas or sparkle:
     print("you have a lighter")
+# with "or" we need only one True condition
 
 if not gas or not sparkle:
     print("you cannot use the lighter")
+# we can use not like in this case to change the status from one argumen from True to False and vice versa.
+
+# python with the comparator "and" analize the first argument and if the first argument (left one) is False, is going to stop and not analize the next one.
+# But with the comparator "or" if the fisrt one is True, is going to stop and not continue analizing the next ones, if the first one is False is going to continue until finds a True and stop in that one.
