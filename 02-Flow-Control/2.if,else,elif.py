e59cdaf6e3d723f3f2f937e1da36e763d788cbcf
GustavoPerpetua <gustavoaperpe@gmail.com>
2023-11-29 16:44:37 -0300

    fixing issues

--- 02-Flow-Control/2.if,else,elif.py
+++ 02-Flow-Control/2.if,else,elif.py
@@ -2,13 +2,12 @@
 
 age = input("Age please :")
 age = int(age)
-if 18 <= age < 64:
+if 65 <= age:
+    print("sorry, you are too old for this stablishment")
+elif 18 < age < 65:
     print("Welcome to the night club")
-elif age > 65:
-    print("Too old to go to parties")
 else:
-    print("You are a minor is not allowed for you")
-
+    print("too young, go home")
 # Only if the previous condition is false will the following be evaluated, first if then elif and last else, from te top to the bottom of the code.
 
 # Ternary
@@ -26,3 +25,14 @@ message = "old enough" if age > 18 else "Too young"
 print(message)
 
 # we can use ternary when you have only one condition
+
+# lets play
+
+if 18 < age < 30:
+    print("what are you going to drink")
+elif 30 <= age < 50:
+    print("you have a bonus 30% discont on drinks ")
+elif 50 <= age < 65:
+    print("Hi sir, thank ypu for coming we have a special promotion 2x1 in champagne")
+else:
+    print("Sorry, but i think you dont supose to be here")
--- 02-Flow-Control/3.operators.py
+++ 02-Flow-Control/3.operators.py
@@ -1,9 +1,8 @@
 # and, or, not
 
 # if we have a lighter we need :
-
-gas = False
-sparkle = True
+gas = bool(input("Do you have gas? True or False?"))
+sparkle = bool(input("Do you have sparkle? True or False?"))
 
 if gas and sparkle:
     print("you can use the lighter")
@@ -19,3 +18,15 @@ if not gas or not sparkle:
 
 # python with the comparator "and" analize the first argument and if the first argument (left one) is False, is going to stop and not analize the next one.
 # But with the comparator "or" if the fisrt one is True, is going to stop and not continue analizing the next ones, if the first one is False is going to continue until finds a True and stop in that one.
+
+
+# lets play
+broken = bool(input("Is your car broken? True or False?"))
+age = int(input("Age: "))
+drink = bool(input("Did you drink? True or False?"))
+car = gas and sparkle and not broken
+
+if 16 < age or not drink and car:
+    print("lets go for a ride")
+else:
+    print("sorry get a taxi")
