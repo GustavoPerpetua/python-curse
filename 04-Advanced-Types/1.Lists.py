47060648e6dfa2c1d230e5253257170627d1684e
GustavoPerpetua <gustavoaperpe@gmail.com>
2023-12-04 23:12:54 -0300

    updt

--- 04-Advanced-Types/1.Lists.py
+++ 04-Advanced-Types/1.Lists.py
@@ -1,2 +1,6 @@
 numbers = [1, 2, 3]
 leters = ["a", "b", "c"]
+words = ["hi", "hello", "hola", "que tal"]
+booleans = [True, False, True, False, False]
+matix = [[0, 0], [0, 1], [1, 0], [1, 1]]
+ceros = [0] * 10
